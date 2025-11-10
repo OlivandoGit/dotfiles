0d977a6f5b315131601509f1f2d695d330d02fb1
OlivandoGit <no-reply@olivando.me>
2025-11-10 19:42:52 +0000

    Setup game display spanning

--- nixos/users/home.nix
+++ nixos/users/home.nix
@@ -9,6 +9,11 @@
     home.username = username;
     home.homeDirectory = "/home/${username}";
 
+    home.sessionPath = [
+        "$HOME/.scripts"
+    ];
+
+
     # Let Home Manager install and manage itself.
     programs.home-manager.enable = true;
 
